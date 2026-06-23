917b607f5012fa80a756b64865cfbab4d1762d85
TanviSurve0005 <tanvisurve1805@gmail.com>
2026-06-23 21:38:25 +0530

    feat: add initial setup for todo application with Redux and Vite
    - Added SVG assets for React and Vite.
    - Created addTodos component for adding new todos.
    - Implemented todos component structure.
    - Established todoSlice for managing todo state with Redux.
    - Added initial CSS styles for the application.
    - Set up main entry point for the application with React and Vite configuration.

---
+++ reduxApp/src/components/todos.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import {addTodo} from '../features/todo/todoSlice'
+
+function todos() {
+
+    
+}
+
+export default todos
